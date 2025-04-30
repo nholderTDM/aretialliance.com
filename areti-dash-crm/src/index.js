d065fa8dbc73f689b206f1a85b777716f488a9df
Nate Holder <natholder01@gmail.com>
2025-04-30 00:39:37 -0400

    Switch from BrowserRouter to HashRouter for routing

--- areti-dash-crm/src/index.js
+++ areti-dash-crm/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
-import './index.css'; // Include if you have this file
+// import './index.css'; // Include if you have this file
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/"'>
+    <HashRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
