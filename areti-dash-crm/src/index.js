b746b5d308fea10ba8cec994425b8267ee5895b5
Nate Holder <natholder01@gmail.com>
2025-04-30 00:24:05 -0400

    Refactor build process and update routing to use BrowserRouter

--- areti-dash-crm/src/index.js
+++ areti-dash-crm/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css'; // Include if you have this file
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename='/"'>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
